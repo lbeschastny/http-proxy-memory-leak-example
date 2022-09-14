f9a24efe62ee74416bf01e7ea48fe6baa9c28ca8
Leonid Beschastny <lbeschastny@gmail.com>
2022-09-14 14:54:15 +0300

    Run http-server in prestart script

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const { createServer } = process.env.MODE === 'http-proxy-middleware'
 	? require('./http-proxy-middleware')
 	: require('./node-http-server');
 
-const target = process.env.TARGET || 'http://127.0.0.1:5000';
-const port = process.env.PORT || 7000
-const memoryUsageInterval = process.env.MEMORY_USAGE_INTERVAL || 10000;
+const httpServerPort = process.env.HTTP_SERVER_PORT || 5000
+const httpProxyPort = process.env.HTTP_PROXY_PORT || 7000
+const target = process.env.HTTP_PROXY_TARGET || `http://127.0.0.1:${httpServerPort}`;
 
-createServer(target, port);
+createServer(target, httpProxyPort);
+
+const memoryUsageInterval = process.env.MEMORY_USAGE_INTERVAL || 10000;
 
 if (memoryUsageInterval > 0) {
 	setInterval(printMemoryUsage, memoryUsageInterval)
